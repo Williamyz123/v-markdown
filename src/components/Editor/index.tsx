0c912a206e1ca9c0e93ca46b6c7265a62ffae63d
Williamyz123 <1326447371@qq.com>
2025-01-22 00:56:28 +0800

    init editor project

---
+++ src/components/Editor/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Editor: React.FC = () => {
+    return (
+        <div className="markdown-editor">
+            这里是编辑器
+            {/* 编辑器内容将在这里实现 */}
+        </div>
+    );
+};
+
+export default Editor;
